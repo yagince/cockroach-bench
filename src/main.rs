e11b1a2c2965c47da8d43e6ba1de9fe01612509f
yagince <straitwalk@gmail.com>
2020-03-01 21:43:28 +0900

    [#master] insert multi threads

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use cockroachdb_bench::{
+    models::{self, NewUser, User},
+    schema,
+};
+use diesel::prelude::*;
+use rayon::prelude::*;
+
+fn main() {
+    let concurrency = 100;
+    let n = 100000;
+
+    rayon::ThreadPoolBuilder::new().num_threads(concurrency as usize).build_global().unwrap();
+
+    let pool = models::create_db_pool(concurrency);
+
+    (1..=n).into_par_iter().for_each(|_|{
+        let conn = pool.get().unwrap();
+
+        let user = conn.transaction(|| {
+            diesel::insert_into(schema::users::table)
+                .values(&NewUser {
+                    name: "hoge".to_owned(),
+                })
+                .get_result::<User>(&conn)
+        });
+        println!("{:?}", user);
+    });
+
+}
